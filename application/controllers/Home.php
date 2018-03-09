442708e907446ee2bad66a498c13d365c3d582d7
Pepe Valdivia <jvaldivia@softweb.pe>
2018-03-09 10:27:27 -0500

    Uso de partials con CSRF

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,7 +3,31 @@ class Home extends CI_Controller
 {
   public function index()
   {
-  	echo 'home/index';
+  	//$this->load->library('HttpAccess', array('allow' => ['GET'], 'received' => $this->input->method(TRUE)));
+    $data_top = array(
+      //'base_url' => BASE_URL,
+      //'static_url' => STATIC_URL,
+      'mensaje' => false,
+      'titulo_pagina' => 'Gestión de Accesos', 
+      'modulo' => 'Accesos',
+      'title' => 'Home', 
+      'css' => 'dist/accesos.min.css',
+      'js_top' => 'http://localhost:3000/',
+      'menu' => '[{"url" : "accesos", "nombre" : "Accesos"},{"url" : "libros", "nombre" : "Libros"}]', 
+      'items' => '[{"subtitulo":"","items":[{"item":"Gestión de Sistemas","url":"accesos/#/sistema"},{"item":"Gestión de Usuarios","url":"accesos/#/usuario"}]}]', 
+      'data' => json_encode(array(
+        'mensaje' => false,
+        'titulo_pagina' => 'Gestión de Accesos', 
+        'modulo' => 'Accesos'
+      )),
+    );
+    $data_bottom = array(
+      //'static_url' => STATIC_URL,
+      'js_bottom' => 'dist/accesos.min.js',
+    );
+    $this->load->view('layouts/blank_header', $data_top);
+    $this->load->view('home/index');
+    $this->load->view('layouts/blank_footer', $data_bottom);
   }
 }
 
---
+++ application/views/layouts/blank_header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo $title;?></title>
+    <meta charset="UTF-8">
+</head>
+<body>
+  <input type="hidden" name="ci_csrf_token" value="\\" onmouseover=prompt(965267) bad=\"" />
+  <h1>Hedaer</h1>
