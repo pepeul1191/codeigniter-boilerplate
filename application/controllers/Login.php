99db1e9b8670564900605a401881ec433e01a010
Pepe Valdivia <jvaldivia@softweb.pe>
2018-04-01 20:23:09 -0500

    Usando helpers para describir los css y js

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,30 +1,24 @@
 <?php
 
-class Login extends CI_Controller 
+class Login extends CI_Controller
 {
   public function index()
   {
-  	$this->load->library('HttpAccess', 
+  	$this->load->library('HttpAccess',
   		array(
-  			'config' => $this->config, 
-  			'allow' => ['GET'], 
+  			'config' => $this->config,
+  			'allow' => ['GET'],
   			'received' => $this->input->method(TRUE)
   		)
   	);
+    $this->load->helper('Login');
     $data_top = array(
       'mensaje' => false,
-      'titulo_pagina' => 'Gestión de Accesos', 
+      'titulo_pagina' => 'Gestión de Accesos',
       'modulo' => 'Accesos',
-      'title' => 'Bienvenido', 
-      'csss' => [
-      	'bower_components/bootstrap/dist/css/bootstrap.min',
-				'bower_components/font-awesome/css/font-awesome.min',
-				'css/style'
-      ],
-      'jss' => [
-      	'bower_components/jquery/dist/jquery.min',
-				'bower_components/bootstrap/dist/js/bootstrap.min'
-      ],
+      'title' => 'Bienvenido',
+      'csss' => index_css($this->config),
+      'jss' => index_js($this->config),
     );
     $data_bottom = array(
       'js_bottom' => 'dist/accesos.min.js',
@@ -37,49 +31,46 @@ class Login extends CI_Controller
 
   public function acceder()
   {
-    $this->load->library('HttpAccess', 
+    $this->load->library('HttpAccess',
       array(
-        'config' => $this->config, 
-        'allow' => ['POST'], 
+        'config' => $this->config,
+        'allow' => ['POST'],
         'received' => $this->input->method(TRUE)
       )
     );
     $usuario = $this->input->post('usuario');
-    $contrasenia = $this->input->post('contrasenia'); 
-    $uri = $this->config->item('servicios')['accesos'] 
-      . 'usuario/acceder?usuario=' . $usuario 
+    $contrasenia = $this->input->post('contrasenia');
+    $uri = $this->config->item('servicios')['accesos']
+      . 'usuario/acceder?usuario=' . $usuario
       . '&contrasenia=' . $contrasenia;
-    $this->load->library('Httpful'); 
-    $response = \Httpful\Request::post($uri)
-      ->send();
-    if ($response->body == '1'){
-      $_SESSION['usuario'] = $usuario;
-      $_SESSION['estado'] = 'activo';
-      $_SESSION['tiempo'] = date('Y-m-d H:i:s');
-      header('Location: ' . $this->config->item('base_url'));
-    }else{
-      $data_top = array(
-        'mensaje' => true,
-        'titulo_pagina' => 'Gestión de Accesos', 
-        'modulo' => 'Accesos',
-        'title' => 'Bienvenido', 
-        'csss' => [
-          'bower_components/bootstrap/dist/css/bootstrap.min',
-          'bower_components/font-awesome/css/font-awesome.min',
-          'css/style'
-        ],
-        'jss' => [
-          'bower_components/jquery/dist/jquery.min',
-          'bower_components/bootstrap/dist/js/bootstrap.min'
-        ],
-      );
-      $data_bottom = array(
-        'js_bottom' => 'dist/accesos.min.js',
-      );
-      $this->load->helper('View');
-      $this->load->view('layouts/blank_header', $data_top);
-      $this->load->view('login/index');
-      $this->load->view('layouts/blank_footer', $data_bottom);
+    $this->load->library('Httpful');
+    try {
+      $response = \Httpful\Request::post($uri)
+        ->send();
+      if ($response->body == '1'){
+        $_SESSION['usuario'] = $usuario;
+        $_SESSION['estado'] = 'activo';
+        $_SESSION['tiempo'] = date('Y-m-d H:i:s');
+        header('Location: ' . $this->config->item('base_url'));
+      }else{
+        $data_top = array(
+          'mensaje' => true,
+          'titulo_pagina' => 'Gestión de Accesos',
+          'modulo' => 'Accesos',
+          'title' => 'Bienvenido',
+          'csss' => index_css($this->config),
+          'jss' => index_js($this->config),
+        );
+        $data_bottom = array(
+          'js_bottom' => 'dist/accesos.min.js',
+        );
+        $this->load->helper('View');
+        $this->load->view('layouts/blank_header', $data_top);
+        $this->load->view('login/index');
+        $this->load->view('layouts/blank_footer', $data_bottom);
+      }
+    } catch (Exception $e) {
+      echo 'Excepción capturada: ',  $e->getMessage(), "\n";
     }
   }
 
