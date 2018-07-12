60125088d721e146b81275c279ccee326d824eba
Pepe Valdivia <jvaldivia@softweb.pe>
2018-07-11 23:24:04 -0500

    Try/catch para listar distritos

--- application/controllers/ubicaciones/Departamento.php
+++ application/controllers/ubicaciones/Departamento.php
@@ -4,19 +4,36 @@ class Departamento extends CI_Controller
 {
   public function listar()
   {
-  	$this->load->library('HttpAccess',
-  		array(
-  			'config' => $this->config,
-  			'allow' => ['GET'],
-  			'received' => $this->input->method(TRUE)
-  		)
-  	);
-    $rs = \Model::factory('\Models\Departamento', 'ubicaciones')
-    	->select('id')
-    	->select('nombre')
-    	->find_array();
+    $this->load->library('HttpAccess',
+      array(
+        'config' => $this->config,
+        'allow' => ['GET'],
+        'received' => $this->input->method(TRUE)
+      )
+    );
+    $rpta = '';
+    $status = 200;
+    try {
+      $rs = \Model::factory('\Models\Departamento', 'ubicaciones')
+      	->select('id')
+      	->select('nombre')
+      	->find_array();
+      $rpta = json_encode($rs);
+    }catch (Exception $e) {
+      $status = 500;
+      $rpta = json_encode(
+        [
+          'tipo_mensaje' => 'error',
+          'mensaje' => [
+  					'No se ha podido listar los departamentos',
+  					$e->getMessage()
+  				]
+        ]
+      );
+    }
     $this->output
-      ->set_output(json_encode($rs));
+      ->set_status_header($status)
+      ->set_output($rpta);
   }
 }
 
